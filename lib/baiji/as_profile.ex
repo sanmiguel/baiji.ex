15b63e0b63d533af45ffbb788c300c8cec3e8489
michael-coles <michael.coles@alertlogic.com>
2017-11-07 14:00:56 +0000

    Add Baiji.AsProfile.perform/1,2
    Pulls profiles from ~/.aws/credentials (as per aws-cli), resolves the
    given profile name and assumes all relevant roles before performing the
    given Operation

---
+++ lib/baiji/as_profile.ex
@@ -0,0 +1,57 @@
+defmodule Baiji.AsProfile do
+  @moduledoc """
+  Derives authentication profiles from local configuration, assumes the
+  roles as appropriate and then performs a request
+  """
+  use Baiji.Auth
+  import Baiji.Core.Utilities
+  alias Baiji.Operation
+
+  def perform(%Operation{} = op, profile \\ :default) do
+    # Read credentials file
+    # Parse profiles
+    # Assume roles in profile stack
+    # Use Baiji.perform
+    assume_stack = fetch(profile)
+    op
+    |> assume_all(assume_stack)
+    |> Baiji.perform
+  end
+
+  def assume_all(%Operation{} = op, roles) do
+    Enum.reduce(roles, op, fn(r, op) ->
+      Baiji.Auth.assume_role(op, r)
+    end)
+  end
+
+  def fetch(profile \\ "default") do
+    # Read ini file
+    home = System.get_env("HOME")
+    path = home <> "/.aws/credentials"
+    # File.exists?(path)
+    {:ok, ini} = File.read(path)
+    profiles = Ini.parse(ini)
+    # Parse and enrich
+    #  - Resolve any source_profile references
+    #  - Store role_arn, account_id for assumption later
+    assume_stack = extract_assume_stack(profile, profiles)
+  end
+
+  def extract_assume_stack(profile, profiles) do
+    case Map.get(profiles, profile) do
+      nil ->
+        # TODO Conversation with Baiji maintainer about appropriate
+        # control flow mechanism for this.
+        exit({:unknown_profile, profile})
+      %{ mfa_serial: _ } ->
+        exit({:unsupported_profile, profile, :mfa_serial})
+      %{ source_profile: source_profile } = p ->
+        %{ role_arn: role } = p
+        # TODO: Optional p = %{ external_id: extid }
+        role_deps = extract_assume_stack(profile, profiles)
+        role_deps ++ [ p ]
+      %{ aws_access_key_id: _, aws_secret_access_key: _ } = profile_data ->
+        [ profile_data ]
+    end
+  end
+end
